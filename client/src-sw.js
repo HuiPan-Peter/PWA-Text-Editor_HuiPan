cf1a040928fbae56129c0b6d5cc35efc8a986dad
HuiPan-Peter <132380239+HuiPan-Peter@users.noreply.github.com>
2023-12-07 17:52:01 -0400

    adding code to apply PWA

--- client/src-sw.js
+++ client/src-sw.js
@@ -24,7 +24,27 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+//Registers the service worker's routes, using the CacheFirst strategy above, then falls back to the offlineFallback page if the user is offline
+registerRoute(({ request }) => request.mode === 'navigate', async ({ event }) => {
+  try {
+    return await pageCache.handle({ event });
+  } catch {
+    return await caches.match(offlineFallback);
+  }
+});
 
-// TODO: Implement asset caching
-registerRoute();
+// This will cache assets like scripts, styles, and images so that they can be served from the cache when the user is offline.
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  
+  // This will cache the assets using the StaleWhileRevalidate strategy, which will return the cached response immediately if available, falling back to the network if it's not cached.
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    // This changes the default maximum age of entries in the cache to 30 days
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
